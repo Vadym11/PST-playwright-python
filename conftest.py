61afcfb3d2c43668af18eeaf619d64fee3128b2f
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-02-18 19:30:23 +0000

    see if viewport works

--- conftest.py
+++ conftest.py
@@ -8,15 +8,15 @@ from utils.test_utils import generate_new_product_data
 def browser_context_args(browser_context_args):
     return {
         **browser_context_args,
-        "viewport": {"width": 1280, "height": 720},
-        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+        "viewport": {"width": 1280, "height": 720}
+        # "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
     }
 
-@pytest.fixture(scope="function")
-def context(context):
-    # Set a global timeout for all actions (60 seconds)
-    context.set_default_timeout(60000)
-    return context
+# @pytest.fixture(scope="function")
+# def context(context):
+#     # Set a global timeout for all actions (60 seconds)
+#     context.set_default_timeout(60000)
+#     return context
 
 @allure.title("Basic API handler")
 @pytest.fixture(scope='module')
