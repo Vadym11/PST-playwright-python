789d64f707535a488a9aba9c9ee505d8fd67cc05
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-02-18 19:38:51 +0000

    return

--- conftest.py
+++ conftest.py
@@ -4,13 +4,13 @@ from AOM.product_api import ProductAPI
 from utils.api_handler import APIHandler
 from utils.test_utils import generate_new_product_data
 
-# @pytest.fixture(scope="session")
-# def browser_context_args(browser_context_args):
-#     return {
-#         **browser_context_args,
-#         "viewport": {"width": 1280, "height": 720},
-#         "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-#     }
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "viewport": {"width": 1280, "height": 720},
+        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    }
 
 @pytest.fixture(scope="function")
 def context(context):
