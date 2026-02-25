efc3a95bd4f9bebec3edaf9ff3f7da081b59eca5
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-02-25 16:35:58 +0000

    base url handling on ci added (#4)
    * base url handling on ci added
    
    * remove branch filter
    
    * add slack message
    
    * slack message fix
    
    * slack message fix 1
    
    * slack message fix 2
    
    * slack message fix 3
    
    * slack message fix 4
    
    * slack message fix 5
    
    * pytest add video
    
    * pytest add video
    
    * fix pytest ini
    
    * change fixtures
    
    * see if viewport works
    
    * see if timeout works
    
    * return
    
    * fix 1
    
    * fix 2
    
    * fix 3
    
    * fix 4
    
    * add postman tests
    
    * remove duplicate
    
    * trigger job
    
    * pass env variable
    
    * small fix
    
    * adding pyright and small refactoring
    
    * change user for login test

--- tests/ui/test_ui_demo.py
+++ tests/ui/test_ui_demo.py
@@ -1,9 +1,16 @@
-import re
-
+import os
 import allure
 import pytest
 from playwright.sync_api import Page, expect
 from lib.pages.home_page import HomePage
+from utils.load_settings import settings
+
+if not os.getenv('WEB_URL'):
+    web_url = settings['web-url_']
+else:
+    web_url = os.getenv('WEB_URL')
+
+print(f'Base web url is set to {web_url}')
 
 @allure.story("Verify Home page")
 @allure.title("Verify Home page loads")
@@ -11,7 +18,7 @@ from lib.pages.home_page import HomePage
 @allure.severity("critical")
 @pytest.mark.Smoke  # mark the test case as smoke
 def test_main_page(page: Page):
-    page.goto("http://localhost:8080")
+    page.goto(web_url)
     expect(page.get_by_test_id('nav-home')).to_be_visible()
     expect(page.get_by_test_id('nav-home')).to_have_text('Home')
 
@@ -25,7 +32,7 @@ def test_main_page(page: Page):
 @allure.severity("critical")
 @pytest.mark.Smoke  # mark the test case as smoke
 def test_login_success(page: Page):
-    page.goto("http://localhost:8080")
+    page.goto(web_url)
 
     page.get_by_test_id('nav-sign-in').click()
 
@@ -34,7 +41,7 @@ def test_login_success(page: Page):
     expect(page.get_by_role('heading', name='Login')).to_be_visible()
     expect(page.get_by_test_id('login-form')).to_be_visible()
 
-    page.get_by_test_id('email').fill('customer@practicesoftwaretesting.com')
+    page.get_by_test_id('email').fill('customer2@practicesoftwaretesting.com')
     page.get_by_test_id('password').fill('welcome01')
 
     page.get_by_test_id('login-submit').click()
@@ -49,7 +56,7 @@ def test_login_success(page: Page):
 @allure.severity("critical")
 @pytest.mark.Smoke  # mark the test case as smoke
 def test_add_to_cart(page: Page):
-    product_page = HomePage(page).go_to().click_random_product()
+    product_page = HomePage(page).go_to(web_url).click_random_product()
 
     product_page.click_add_to_cart_and_assert_pop_ups()
 
