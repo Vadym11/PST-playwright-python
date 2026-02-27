629d7c2c6e7bce2ed0dfb0cad18ceba6309581c1
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-02-27 22:34:05 +0000

    PS-32-Build-custom-docker-image (#8)
    * use custom docker image
    
    * trigger build
    
    * try v1-cimg image
    
    * change java_home
    
    * change java_home 1
    
    * change java_home 1 - move comments
    
    * change java_home 1 - reformat yaml
    
    * use new image + caching
    
    * change cache v1 -> v2
    
    * change cache v1 -> v2 1
    
    * change cache v1 -> v2 2
    
    * move cache to playwright block
    
    * trigger build
    
    * remove redundant dependencies installation
    
    * try manual caching for type checking
    
    * unify type checking and playwright tests
    
    * trigger build

--- tests/ui/auth/test_register_new_user.py
+++ tests/ui/auth/test_register_new_user.py
@@ -11,4 +11,5 @@ def test_register_new_user_happy_path(page: Page, random_user_data):
      .click_register_link()
      .enter_registration_details(random_user_data)
      .click_register_button()
+    # if registration is successful, it should navigate to the login page, which we assert here
      .assert_login_page())
