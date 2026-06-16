71ddbc198a75fefeb4e7fe87b736c1ada103a35b
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-06-16 12:14:30 +0100

    fix: base_url issue

--- lib/pages/home_page.py
+++ lib/pages/home_page.py
@@ -14,9 +14,9 @@ class HomePage(BasePage):
         super().__init__(page)
         self.header = HeaderCommon(page)
 
-    def go_to(self) -> HomePage:
+    def go_to(self, url: str) -> HomePage:
         # Use _page to match BasePage
-        self._page.goto(base_api_url)
+        self._page.goto(url)
         self._page.get_by_role("menubar", name="Main menu").wait_for(state="visible")
 
         return self
--- tests/ui/auth/test_register_new_user.py
+++ tests/ui/auth/test_register_new_user.py
@@ -2,10 +2,10 @@ from playwright.sync_api import Page
 from lib.api_models.user import CreateUser
 from lib.pages.home_page import HomePage
 
-def test_register_new_user_happy_path(page: Page, random_user_data: CreateUser):
+def test_register_new_user_happy_path(page: Page, random_user_data: CreateUser, base_url: str):
     """Register a new user."""
 
-    home_page = HomePage(page).go_to()
+    home_page = HomePage(page).go_to(base_url)
     sign_in_page = home_page.header.click_sign_in_link()
 
     (sign_in_page
